2006a73a4341f2a497507e108986d7d427ccd614
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-11-15 01:07:16 +0600

    initially setup done

---
+++ src/Components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderMain = () => {
+  return (
+    <div>
+      <h2>Header main</h2>
+    </div>
+  );
+};
+
+export default HeaderMain;
---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../Shared/NavBar/NavBar";
+import HeaderMain from "../HeaderMain/HeaderMain";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <HeaderMain></HeaderMain>
+    </div>
+  );
+};
+
+export default Home;
