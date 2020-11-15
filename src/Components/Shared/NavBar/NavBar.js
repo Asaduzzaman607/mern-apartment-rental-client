04714662237bce4a3082b22c6a7dc526253566cb
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-11-16 00:35:43 +0600

    done with signup , login  page and dynamic banner also

--- src/Components/Shared/NavBar/NavBar.js
+++ src/Components/Shared/NavBar/NavBar.js
@@ -12,51 +12,39 @@ const NavBar = () => {
               <Link className="navbar-brand ml-5" href="#">
                 <img src={logo} alt="" className="img-fluid" />
               </Link>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-
               <div
                 className="collapse navbar-collapse"
                 id="navbarSupportedContent"
               >
                 <ul className="navbar-nav ml-auto">
                   <li className="nav-item active">
-                    <Link className="nav-link" href="#">
+                    <Link className="nav-link" to="/home">
                       Home
                     </Link>
                   </li>
                   <li className="nav-item active">
-                    <Link className="nav-link" href="#">
+                    <Link className="nav-link" to="/about">
                       About
                     </Link>
                   </li>
                   <li className="nav-item active">
-                    <Link className="nav-link" href="#">
+                    <Link className="nav-link" to="/services">
                       Services
                     </Link>
                   </li>
                   <li className="nav-item active">
-                    <Link className="nav-link" href="#">
+                    <Link className="nav-link" to="/event">
                       Event
                     </Link>
                   </li>
                   <li className="nav-item active">
-                    <Link className="nav-link" href="#">
+                    <Link className="nav-link" to="/contact">
                       Contact
                     </Link>
                   </li>
-                  <li className="nav-item active mr-5 pr-5">
-                    <button className="green-btn ">Login</button>
-                  </li>
+                  <Link className=" mr-5 pr-5" to="/login">
+                    <button className="green-btn">Login</button>
+                  </Link>
                 </ul>
               </div>
             </nav>
