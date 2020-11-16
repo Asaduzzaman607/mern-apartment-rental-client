84a66e1430111e90adb1e7fcf5f247df994283b5
Web Kawsar <web.kawsarahmed@gmail.com>
2020-11-16 16:27:08 +0600

    done house details booking form

--- src/Components/BookHouse/BookingForm/BookingForm.js
+++ src/Components/BookHouse/BookingForm/BookingForm.js
@@ -1,48 +1,97 @@
 import { Box, Paper, TextField } from '@material-ui/core';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import "./BookingForm.scss";
 
 
 
 const BookingForm = () => {
+
+    
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
         <>
             <Box className="form">
-                <Paper>
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                         <TextField
+                            inputRef={register({
+                                required: "Full name is required",
+                            })}
                             fullWidth
                             id="name"
                             label="Full Name"
-                            helperText=""
+                            margin="normal"
                             variant="outlined"
+                            type="text"
+                            className="input"
+                            name="fullName"
+                            FormHelperTextProps={{
+                                
+                            }}
+                            helperText={errors.fullName && errors.fullName.message}
                         />
+
+                        
+
                         <TextField
+                            inputRef={register({
+                                required: "Phone number is required",
+                            })}
                             fullWidth
                             id="phone"
                             label="Phone No"
-                            helperText=""
+                            className="input"
+                            margin="normal"
+                            type="number"
+                            name="phone"
                             variant="outlined"
+                            FormHelperTextProps={{
+                                
+                            }}
+                            helperText={errors.phone && errors.phone.message}
                         />
                         <TextField
+                            inputRef={register({
+                                required: "Email is required",
+                            })}
                             fullWidth
                             id="email"
+                            className="input"
                             label="Email"
-                            helperText=""
+                            margin="normal"
+                            type="email"
+                            name="email"
                             variant="outlined"
+                            FormHelperTextProps={{
+                                
+                            }}
+                            helperText={errors.email && errors.email.message}
                         />
                         <TextField
+                            inputRef={register({
+                                required: "Message is required",
+                            })}
                             fullWidth
                             id="message"
                             label="Message"
+                            className="input"
+                            margin="normal"
+                            type="text"
                             multiline
+                            name="message"
                             rows={6}
-                            helperText=""
                             variant="outlined"
+                            FormHelperTextProps={{
+                                
+                            }}
+                            helperText={errors.message && errors.message.message}
                         />
+                        
                         <input className="submit" type="submit" value="Request Booking"/>
                     </form>
-                </Paper>
             </Box>
         </>
     );
